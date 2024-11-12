45ddb46de3175035ec7a634b461f76a66a0fd3b1
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-12 23:25:44 +0200

    [FEAT]: Add new dependencies

--- init_win.ps1
+++ init_win.ps1
@@ -3,4 +3,4 @@ $AppHome=$PSScriptRoot
 python -m venv --clear $AppHome/.venv
 .$AppHome/.venv/Scripts/Activate.ps1
 python -m pip install --upgrade pip==23.2.1
-python -m pip install -e '.[test]' --extra-index-url https://nexus.gfk.com/repository/pypi_sqc_hosted/simple/
+python -m pip install -e '.[test]' 
