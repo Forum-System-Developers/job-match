06fc6c70b49612e61df09ee64397ba86da9fcbf4
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-13 19:43:17 +0200

    [FEAT]: Add JobAdsRequirement model.

---
+++ src/app/sql_app/job_ad_requirement/job_ads_requirement.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, DateTime, ForeignKey, func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from src.app.sql_app.database import Base
+
+
+class JobAdsRequirement(Base):
+    """
+    JobAdsRequirement is a SQLAlchemy model representing the requirements for job advertisements.
+
+    Attributes:
+        job_ad_id (UUID): Foreign key referencing the ID of the job advertisement.
+        job_application_id (UUID): Foreign key referencing the ID of the job application.
+        created_at (DateTime): Timestamp when the record was created, with timezone support.
+        updated_at (DateTime): Timestamp when the record was last updated, with timezone support.
+
+    Relationships:
+        job_ad (relationship): Relationship to the JobAd model.
+        job_application (relationship): Relationship to the JobApplication model.
+    """
+
+    __tablename__ = "job_ads_requirements"
+
+    job_ad_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    job_application_id = Column(
+        UUID(as_uuid=True), ForeignKey("users.id"), nullable=False
+    )
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    job_ad = relationship("JobAd", back_populates="job_ads_requirements")
+    job_application = relationship(
+        "JobApplication", back_populates="job_ads_requirements"
+    )
