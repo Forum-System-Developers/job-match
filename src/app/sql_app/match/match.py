fa5fa8e46a6b452ee24f04b15cdca12b7709a8cc
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-14 12:11:49 +0200

    [FEAT]: Add Match model.

---
+++ src/app/sql_app/match/match.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, DateTime, ForeignKey, func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from src.app.sql_app.database import Base
+
+
+class Match(Base):
+    """
+    Represents a match between a job advertisement and a job application.
+
+    Attributes:
+        job_ad_id (UUID): The unique identifier of the job advertisement.
+        job_application_id (UUID): The unique identifier of the job application.
+        created_at (DateTime): The timestamp when the match was created.
+
+    Relationships:
+        job_ad (JobAd): The job advertisement associated with this match.
+        job_application (JobApplication): The job application associated with this match.
+    """
+
+    __tablename__ = "matches"
+
+    job_ad_id = Column(UUID(as_uuid=True), ForeignKey("job_ads.id"), primary_key=True)
+    job_application_id = Column(
+        UUID(as_uuid=True), ForeignKey("job_applications.id"), primary_key=True
+    )
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    job_ad = relationship("JobAd", back_populates="matches")
+    job_application = relationship("JobApplication", back_populates="matches")
