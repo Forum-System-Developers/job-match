a3860dabb1c688542bfc67a801266722784ff04f
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-14 17:37:51 +0200

    [FIX] Remove reduntant code

--- src/app/api/api_v1/api.py
+++ src/app/api/api_v1/api.py
@@ -2,18 +2,18 @@
 
 from fastapi import APIRouter
 
-from src.app.api.api_v1.endpoints import hello_world, items
+# from src.app.api.api_v1.endpoints import hello_world, items
 
 api_router = APIRouter()
 
-api_router.include_router(
-    hello_world.router,
-    prefix="/hello-world",
-    tags=["Hello World"],
-)
+# api_router.include_router(
+#     hello_world.router,
+#     prefix="/hello-world",
+#     tags=["Hello World"],
+# )
 
-api_router.include_router(
-    items.router,
-    prefix="",
-    tags=["Users"],
-)
+# api_router.include_router(
+#     items.router,
+#     prefix="",
+#     tags=["Users"],
+# )
