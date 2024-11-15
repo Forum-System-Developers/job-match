4e05c1b3ae1b06f76bafa901800d70b1abb13e05
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-15 16:45:21 +0200

    [FEAT]: Add location property in BaseJobAd schema.

--- src/app/schemas/job_ad.py
+++ src/app/schemas/job_ad.py
@@ -9,6 +9,7 @@ from src.app.sql_app.job_ad.job_ad_status import JobAdStatus
 class BaseJobAd(BaseModel):
     title: str
     description: str
+    location: str
     min_salary: condecimal(gt=0, max_digits=10, decimal_places=2)  # type: ignore
     max_salary: condecimal(gt=0, max_digits=10, decimal_places=2)  # type: ignore
 
