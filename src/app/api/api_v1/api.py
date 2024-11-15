c4f7977fec8298269cecd6a65ce3cf19a62c88e5
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-15 18:10:24 +0200

    [FEAT] Add professional_router

--- src/app/api/api_v1/api.py
+++ src/app/api/api_v1/api.py
@@ -3,6 +3,7 @@
 from fastapi import APIRouter
 
 from src.app.api.api_v1.endpoints import job_ad
+from src.app.api.api_v1.endpoints import profeesional_router
 
 api_router = APIRouter()
 
@@ -10,4 +11,8 @@ api_router.include_router(
     job_ad.router,
     prefix="/job-ad",
     tags=["Job Ad"],
+)
+
+api_router.include_router(
+    profeesional_router.router, prefix="/professionals", tags=["Professionals"]
 )
