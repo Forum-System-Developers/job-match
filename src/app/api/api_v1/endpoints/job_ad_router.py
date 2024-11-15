0c5e4d231b986d7bbd6ec57331b812032a989e83
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-15 16:31:41 +0200

    [FEAT]: Add create_job_ad router.

--- src/app/api/api_v1/endpoints/job_ad_router.py
+++ src/app/api/api_v1/endpoints/job_ad_router.py
@@ -2,11 +2,10 @@ from typing import Union
 from uuid import UUID
 
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
-from src.app.schemas.job_ad import JobAdResponse
+from src.app.schemas.job_ad import JobAdCreate, JobAdResponse
 from src.app.services import job_ad_service
 from src.app.sql_app.database import get_db
 from src.app.utils.process_request import process_request
@@ -21,11 +20,28 @@ router = APIRouter()
 )
 def get_job_ad_by_id(
     id: UUID, db: Session = Depends(get_db)
-) -> BaseModel | JSONResponse:
+) -> Union[JobAdResponse, JSONResponse]:
     def _get_job_ad_by_id():
         return job_ad_service.get_by_id(id=id, db=db)
 
     return process_request(
         get_entities_fn=_get_job_ad_by_id,
         not_found_err_msg=f"Job Ad with id {id} not found",
+    )
+
+
+@router.post(
+    "/",
+    response_model=Union[JobAdResponse, JSONResponse],
+    description="Create a new job advertisement.",
+)
+def create_job_ad(
+    job_ad_data: JobAdCreate, db: Session = Depends(get_db)
+) -> Union[JobAdResponse, JSONResponse]:
+    def _create_job_ad():
+        return job_ad_service.create(job_ad_data=job_ad_data, db=db)
+
+    return process_request(
+        get_entities_fn=_create_job_ad,
+        not_found_err_msg="Job Ad not created",
     )
