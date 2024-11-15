ca9d86b845780c1add467c32146622b5e3ebd2be
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-16 01:33:04 +0200

    [FEAT] Add update professional endpoint

--- src/app/api/api_v1/endpoints/professional_router.py
+++ src/app/api/api_v1/endpoints/professional_router.py
@@ -13,7 +13,7 @@ router = APIRouter()
 
 
 @router.post(
-    "/create",
+    "/",
     response_model=ProfessionalResponse,
     description="Create a profile for a Professional.",
 )
@@ -30,7 +30,7 @@ def create(
     Args:
         professional (ProfessionalBase): The professional's details from the request body.
         status (ProfessionalStatus): Status of the professional - Active/Busy.
-        photo (Optional[UploadFile]): The professional's photo (if provided).
+        photo (UploadFile | None): The professional's photo (if provided).
         db (Session): Database session dependency.
 
     Returns:
@@ -49,4 +49,44 @@ def create(
     return process_request(
         get_entities_fn=_create,
         not_found_err_msg="Professional could not be created",
+    )
+
+
+@router.put(
+    "/",
+    response_model=ProfessionalResponse,
+    description="Update a profile for a Professional.",
+)
+def update(
+    professional: ProfessionalBase,
+    status: ProfessionalStatus = Form(),
+    photo: UploadFile | None = File(None),
+    # user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> ProfessionalResponse:
+    """
+    Update a professional profile.
+
+    Args:
+        professional (ProfessionalBase): The professional's details from the request body.
+        status (ProfessionalStatus): Status of the professional - Active/Busy.
+        photo (UploadFile | None): The professional's photo (if provided).
+        db (Session): Database session dependency.
+
+    Returns:
+        ProfessionalResponse: The created professional profile response.
+    """
+
+    def _update():
+        return professional_service.update(
+            # user=user,
+            professional=professional,
+            status=status,
+            db=db,
+            photo=photo,
+        )
+
+    return process_request(
+        get_entities_fn=_update,
+        not_found_err_msg="Professional could not be updated",
     )
