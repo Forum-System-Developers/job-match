793bbf3af2fd4a648a2ae8a0796f4b7ad99109a9
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-17 00:56:51 +0200

    [FEAT] Enhance JobApplication schema

--- src/app/schemas/job_application.py
+++ src/app/schemas/job_application.py
@@ -1,33 +1,40 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
-
-from app.sql_app.job_application.job_application_status import JobStatus
+from pydantic import BaseModel, Field, model_validator
 
 
 class JobAplicationBase(BaseModel):
     """
-    Request body model for creating or updating a professional's job application.
+    Model for creating or updating a professional's job application.
 
-    This model is used to capture the basic information required to reate or update a job application. It includes required attributes such as the description, job Applicant skills, whether it will be the main application. The data should be passed as a JSON object in the request body.
+    This model is used to capture the basic information required to reate or update a job application. It includes the required attributes description, application status, and a list of Applicant skills. Optional attributes are minimum and maximum salary range The data should be passed as a JSON object in the request body.
 
     Attributes:
         description (str): Description of the professional.
         city (str): The city the professional is located in.
     """
 
-    first_name: str = Field(examples=["Jane"])
-    last_name: str = Field(examples=["Doe"])
+    min_salary: int | None = Field(ge=0, description="Minimum salary (>= 0)")
+    max_salary: int | None = Field(ge=0, description="Maximum salary (>= 0)")
+
     description: str = Field(
         examples=["A seasoned web developer with expertise in FastAPI"]
     )
-    city: str = Field(examples=["Sofia"])
+    skills: list[str] = Field(examples=[["Python", "Linux", "React"]])
+
+    @model_validator(mode="before")
+    def validate_salary_range(cls, values):
+        min_salary = values.get("min_salary")
+        max_salary = values.get("max_salary")
+        if min_salary > max_salary:
+            raise ValueError("min_salary must be less than or equal to max_salary")
+        return values
 
     class Config:
         from_attributes = True
 
 
-class JobApplciationResponse(JobAplicationBase):
+class JobApplicationResponse(JobAplicationBase):
     """
     Pydantic schema representing the FastAPI response for Job Application.
 
@@ -45,32 +52,3 @@ class JobApplciationResponse(JobAplicationBase):
 
     class Config:
         from_attributes = True
-
-
-class FilterParams(BaseModel):
-    """
-    Pydantic schema for pagination and filtering parameters.
-
-    This schema is designed to handle standard query parameters
-    for limiting and offsetting results in paginated responses.
-
-    Attributes:
-        limit (int): The maximum number of records to return.
-            - Default: 100
-            - Constraints: Must be greater than 0 and less than or equal to 100.
-        offset (int): The number of records to skip before starting to return results.
-            - Default: 0
-            - Constraints: Must be greater than or equal to 0.
-
-    Example:
-        Use this schema in FastAPI endpoints to simplify pagination:
-
-        ```
-        @app.get("/items/")
-        def get_items(filter_params: Annotated[FilterParams, Query()] = FilterParams()):
-            ...
-        ```
-    """
-
-    limit: int = Field(10, gt=0, le=100)
-    offset: int = Field(0, ge=0)
