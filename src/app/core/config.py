0a4f6ffe40b19cf2ea6d20ef3d43caf6dade4607
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-17 00:56:04 +0200

    [FIX] Update environment file path

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -33,7 +33,7 @@ class Settings(BaseSettings):
 
     class Config:
         case_sensitive = True
-        env_file = "../.env"
+        env_file = ".env"
         env_file_encoding = "utf-8"
         allow_extra = True
 
