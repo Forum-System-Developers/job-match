a18596932befd993ef1a15b3365884460f535cf4
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-16 12:09:50 +0200

    [REFACTOR] Fix FilterParams attributes and update docstrings

--- src/app/schemas/professional.py
+++ src/app/schemas/professional.py
@@ -53,7 +53,29 @@ class ProfessionalResponse(ProfessionalBase):
 
 
 class FilterParams(BaseModel):
+    """
+    Pydantic schema for pagination and filtering parameters.
+
+    This schema is designed to handle standard query parameters
+    for limiting and offsetting results in paginated responses.
+
+    Attributes:
+        limit (int): The maximum number of records to return.
+            - Default: 100
+            - Constraints: Must be greater than 0 and less than or equal to 100.
+        offset (int): The number of records to skip before starting to return results.
+            - Default: 0
+            - Constraints: Must be greater than or equal to 0.
+
+    Example:
+        Use this schema in FastAPI endpoints to simplify pagination:
+
+        ```
+        @app.get("/items/")
+        def get_items(filter_params: Annotated[FilterParams, Query] = FilterParams()):
+            ...
+        ```
+    """
+
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
-    # order_by: Literal["created_at", "updated_at"] = "created_at"
-    tags: list[str] = []
