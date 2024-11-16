cb5375735a45786ff2fb3ff1da1ecee25e78870f
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-16 21:01:41 +0200

    [FEAT]: Update company endpoints to return JSON responses and include status codes.

--- src/app/api/api_v1/endpoints/company_router.py
+++ src/app/api/api_v1/endpoints/company_router.py
@@ -1,29 +1,29 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
-from app.schemas.company import CompanyResponse
 from app.services import company_service
-from src.app.sql_app.database import get_db
-from src.app.utils.process_request import process_request
+from app.sql_app.database import get_db
+from app.utils.process_request import process_request
 
 router = APIRouter()
 
 
 @router.get(
     "/",
-    response_model=list[CompanyResponse],
     description="Retrieve all companies.",
 )
 def get_all_companies(
     skip: int = 0, limit: int = 50, db: Session = Depends(get_db)
-) -> list[CompanyResponse]:
+) -> JSONResponse:
     def _get_all_companies():
         return company_service.get_all(db=db, skip=skip, limit=limit)
 
     return process_request(
         get_entities_fn=_get_all_companies,
+        status_code=status.HTTP_200_OK,
         not_found_err_msg="No companies found",
     )
 
@@ -32,11 +32,12 @@ def get_all_companies(
     "/{id}",
     description="Retrieve a company by its unique identifier.",
 )
-def get_company_by_id(id: UUID, db: Session = Depends(get_db)) -> CompanyResponse:
+def get_company_by_id(id: UUID, db: Session = Depends(get_db)) -> JSONResponse:
     def _get_company_by_id():
         return company_service.get_by_id(id=id, db=db)
 
     return process_request(
         get_entities_fn=_get_company_by_id,
+        status_code=status.HTTP_200_OK,
         not_found_err_msg=f"Company with id {id} not found",
     )
