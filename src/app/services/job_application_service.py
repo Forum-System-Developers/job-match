1b6dda8875542146746852c727877d352123e89b
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-17 03:21:45 +0200

    [FEATURE] Implement create functionality

--- src/app/services/job_application_service.py
+++ src/app/services/job_application_service.py
@@ -1,15 +1,11 @@
 import logging
-from uuid import UUID
 
-from fastapi import status
 from sqlalchemy.orm import Session
 
-from app.exceptions.custom_exceptions import ApplicationError
-from app.sql_app.professional.professional import Professional
 from app.sql_app.job_application.job_application import JobApplication
 from app.sql_app.job_application.job_application_status import JobStatus
 from app.schemas.job_application import JobAplicationBase, JobApplicationResponse
-from app.services import professional_service
+from app.services import address_service, professional_service
 from app.schemas.user import UserResponse
 
 logger = logging.getLogger(__name__)
@@ -35,4 +31,18 @@ def create(
     Returns:
         JobApplicationResponse: JobApplication Pydantic response model.
     """
+    city = address_service.get_by_name(name=application.city, db=db)
     professional = professional_service.get_by_id(professional_id=user.id, db=db)
+    job_application = JobApplication(
+        **application.model_dump(exclude="city"),
+        is_main=is_main,
+        status=application_status,
+        city_id=city.id,
+        professional_id=user.id,
+    )
+
+    logger.info(f"Job Application {job_application} created")
+
+    return JobApplicationResponse.create(
+        professional=professional, job_application=job_application, city=city.name
+    )
