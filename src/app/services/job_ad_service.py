3b0b461c389c6cbfea53a22f00580bd8b6b02cdd
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-17 05:15:47 +0200

    [REFACTOR]: Replace internal validation functions with external validators for job ads, companies, and locations.

--- src/app/services/job_ad_service.py
+++ src/app/services/job_ad_service.py
@@ -2,13 +2,14 @@ import logging
 from datetime import datetime, timezone
 from uuid import UUID
 
-from fastapi import status
 from sqlalchemy.orm import Session
 
-from app.exceptions.custom_exceptions import ApplicationError
 from app.schemas.job_ad import JobAdCreate, JobAdResponse, JobAdUpdate
-from app.sql_app.city.city import City
-from app.sql_app.company.company import Company
+from app.services.utils.validators import (
+    ensure_valid_company_id,
+    ensure_valid_job_ad_id,
+    ensure_valid_location,
+)
 from app.sql_app.job_ad.job_ad import JobAd
 from app.sql_app.job_ad.job_ad_status import JobAdStatus
 
@@ -44,7 +45,7 @@ def get_by_id(id: UUID, db: Session) -> JobAdResponse:
     Returns:
         Optional[JobAdResponse]: The job advertisement if found, otherwise None.
     """
-    job_ad = _ensure_valid_job_ad_id(id=id, db=db)
+    job_ad = ensure_valid_job_ad_id(id=id, db=db)
     logger.info(f"Retrieved job ad with id {id}")
 
     return JobAdResponse.model_validate(job_ad)
@@ -64,7 +65,7 @@ def create(job_ad_data: JobAdCreate, db: Session) -> JobAdResponse:
     Raises:
         ApplicationError: If the company or city is not found.
     """
-    _ensure_valid_company_id(id=job_ad_data.company_id, db=db)
+    ensure_valid_company_id(id=job_ad_data.company_id, db=db)
     job_ad = JobAd(**job_ad_data.model_dump(), status=JobAdStatus.ACTIVE)
 
     db.add(job_ad)
@@ -90,7 +91,7 @@ def update(id: UUID, job_ad_data: JobAdUpdate, db: Session) -> JobAdResponse:
     Raises:
         ApplicationError: If the job advertisement, city, or company is not found.
     """
-    job_ad = _ensure_valid_job_ad_id(id=id, db=db)
+    job_ad = ensure_valid_job_ad_id(id=id, db=db)
     job_ad = _update_job_ad(job_ad_data=job_ad_data, job_ad=job_ad, db=db)
 
     if any(value is None for value in vars(job_ad_data).values()):
@@ -103,13 +104,20 @@ def update(id: UUID, job_ad_data: JobAdUpdate, db: Session) -> JobAdResponse:
     return JobAdResponse.model_validate(job_ad)
 
 
-def _get_by_id(id: UUID, db: Session) -> JobAd | None:
-    return db.query(JobAd).filter(JobAd.id == id).first()
-
-
 def _update_job_ad(job_ad_data: JobAdUpdate, job_ad: JobAd, db: Session) -> JobAd:
+    """
+    Updates the fields of a job advertisement based on the provided job_ad_data.
+
+    Args:
+        job_ad_data (JobAdUpdate): An object containing the updated data for the job advertisement.
+        job_ad (JobAd): The job advertisement object to be updated.
+        db (Session): The database session used for validating the location.
+
+    Returns:
+        JobAd: The updated job advertisement object.
+    """
     if job_ad_data.location is not None:
-        _ensure_valid_location(location=job_ad_data.location, db=db)
+        ensure_valid_location(location=job_ad_data.location, db=db)
         logger.info(f"Updated job ad (id: {id}) location to {job_ad_data.location}")
 
     if job_ad_data.title is not None:
@@ -135,36 +143,3 @@ def _update_job_ad(job_ad_data: JobAdUpdate, job_ad: JobAd, db: Session) -> JobA
         logger.info(f"Updated job ad (id: {id}) status to {job_ad_data.status}")
 
     return job_ad
-
-
-def _ensure_valid_location(location: str, db: Session) -> City:
-    city = db.query(City).filter(City.name == location).first()
-    if city is None:
-        logger.error(f"City with name {location} not found")
-        raise ApplicationError(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"City with name {location} not found",
-        )
-    return city
-
-
-def _ensure_valid_job_ad_id(id: UUID, db: Session) -> JobAd:
-    job_ad = _get_by_id(id=id, db=db)
-    if job_ad is None:
-        logger.error(f"Job Ad with id {id} not found")
-        raise ApplicationError(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Job Ad with id {id} not found",
-        )
-    return job_ad
-
-
-def _ensure_valid_company_id(id: UUID, db: Session) -> Company:
-    company = db.query(Company).filter(Company.id == id).first()
-    if company is None:
-        logger.error(f"Company with id {id} not found")
-        raise ApplicationError(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Company with id {id} not found",
-        )
-    return company
