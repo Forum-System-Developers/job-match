6f7b628d0bc637538dc26978bfc7ff3dbc51f9ae
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-17 02:39:57 +0200

    [FEAT] Update create function

--- src/app/api/api_v1/endpoints/job_application_router.py
+++ src/app/api/api_v1/endpoints/job_application_router.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, File, Form, UploadFile
+from fastapi import APIRouter, Depends, Form
 from fastapi import status as status_code
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
@@ -12,17 +12,20 @@ from app.services.auth_service import get_current_user
 from app.sql_app.database import get_db
 from app.sql_app.user.user import User
 from app.utils.process_request import process_request
+from app.sql_app.job_application.job_application_status import JobStatus
+
 
 router = APIRouter()
 
 
 @router.post(
     "/",
-    description="Create a profile for a Professional.",
+    description="Create a Job Application.",
 )
 def create(
     application: JobAplicationBase,
-    is_main: bool = Form(default=True, description="Default send value is True"),
+    is_main: bool = Form(description="Set the Job application as main"),
+    application_status: JobStatus = Form(description="Status of the Job Application"),
     user: User = Depends(get_current_user),
     db: Session = Depends(get_db),
 ) -> JSONResponse:
@@ -33,16 +36,20 @@ def create(
         professional (ProfessionalBase): The professional's details from the request body.
         status (ProfessionalStatus): Status of the professional - Active/Busy.
         photo (UploadFile | None): The professional's photo (if provided).
-        user (User): The current logged in User.
+        user (UserResponse): The current logged in User.
         db (Session): Database session dependency.
 
     Returns:
-        ProfessionalResponse | JSONResponse: The created professional profile response.
+        JSONResponse: The created professional profile response.
     """
 
     def _create():
         return professional_service.create(
-            user=user, application=application, is_main=is_main, db=db
+            user=user,
+            application=application,
+            is_main=is_main,
+            application_status=application_status,
+            db=db,
         )
 
     return process_request(
