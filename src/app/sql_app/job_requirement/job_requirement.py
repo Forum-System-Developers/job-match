b278ea9d8a5c4d159df841e25ac99ab15dd14e50
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-18 19:01:08 +0200

    [FEAT]: Enhance JobRequirement model by adding company_id.

--- src/app/sql_app/job_requirement/job_requirement.py
+++ src/app/sql_app/job_requirement/job_requirement.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import DateTime, Enum, String, func
+from sqlalchemy import DateTime, Enum, ForeignKey, String, func
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -15,17 +15,18 @@ if TYPE_CHECKING:
 
 class JobRequirement(Base):
     """
-    Represents a job requirement in the database.
+    Represents a job requirement in the job matching application.
 
     Attributes:
-        id (UUID): Unique identifier for the job requirement.
+        id (uuid.UUID): Unique identifier for the job requirement.
+        company_id (uuid.UUID): Identifier for the associated company. Can be null.
         description (str): Description of the job requirement.
-        skill_level (SkillLevel): The skill level required for the job.
+        skill_level (SkillLevel): Required skill level for the job.
         created_at (datetime): Timestamp when the job requirement was created.
         updated_at (datetime): Timestamp when the job requirement was last updated.
 
     Relationships:
-        job_ad_requirements (relationship): Relationship to the JobAdsRequirement model.
+        job_ad_requirements (list[JobAdsRequirement]): List of job ad requirements associated with this job requirement.
     """
 
     __tablename__ = "job_requirement"
@@ -36,6 +37,9 @@ class JobRequirement(Base):
         primary_key=True,
         unique=True,
         nullable=False,
+    )
+    company_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("company.id"), nullable=True
     )
     description: Mapped[str] = mapped_column(String, nullable=False)
     skill_level: Mapped[SkillLevel] = mapped_column(Enum(SkillLevel), nullable=False)
