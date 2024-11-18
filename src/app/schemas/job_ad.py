f408724e121645db1a2c918f95743d221a6be764
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-18 21:25:27 +0200

    [FEAT]: Add requirements field to JobAdResponse and add factory method.

--- src/app/schemas/job_ad.py
+++ src/app/schemas/job_ad.py
@@ -4,6 +4,8 @@ from uuid import UUID
 from pydantic import BaseModel, condecimal
 
 from app.schemas.custom_types import Salary
+from app.schemas.skill import Requirement
+from app.sql_app.job_ad.job_ad import JobAd
 from app.sql_app.job_ad.job_ad_status import JobAdStatus
 
 
@@ -21,8 +23,26 @@ class BaseJobAd(BaseModel):
 class JobAdResponse(BaseJobAd):
     id: UUID
     status: JobAdStatus
+    requirements: list[Requirement] = []
     created_at: datetime
     updated_at: datetime
+
+    @classmethod
+    def create(cls, job_ad: JobAd) -> "JobAdResponse":
+        return cls(
+            id=job_ad.id,
+            title=job_ad.title,
+            description=job_ad.description,
+            location=job_ad.location.name,
+            min_salary=job_ad.min_salary,
+            max_salary=job_ad.max_salary,
+            status=job_ad.status,
+            requirements=[
+                Requirement.model_validate(req) for req in job_ad.job_ads_requirements
+            ],
+            created_at=job_ad.created_at,
+            updated_at=job_ad.updated_at,
+        )
 
 
 class JobAdCreate(BaseJobAd):
