f5f58a404e80ee654a5dca41054e2c560e2e2781
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-18 17:00:04 +0200

    [REFACTOR]: Clean up import statements

--- src/app/services/match_service.py
+++ src/app/services/match_service.py
@@ -3,32 +3,12 @@ from uuid import UUID
 
 from fastapi import status
 from sqlalchemy.orm import Session
-from sqlalchemy.orm.query import RowReturningQuery
 
 from app.exceptions.custom_exceptions import ApplicationError
-from app.schemas.address import CityResponse
-from app.schemas.common import FilterParams, SearchParams
-from app.schemas.job_application import (
-    JobAplicationBase,
-    JobApplicationResponse,
-    JobSearchStatus,
-)
-from app.schemas.job_application import JobStatus as JobStatusInput
-from app.schemas.professional import ProfessionalResponse
-from app.schemas.skill import SkillBase
-from app.schemas.user import UserResponse
-from app.services import (
-    city_service,
-    job_ad_service,
-    professional_service,
-    skill_service,
-)
+from app.schemas.job_application import JobStatus
 from app.sql_app.job_ad.job_ad_status import JobAdStatus
-from app.sql_app.job_application.job_application_status import JobStatus
-from app.sql_app.job_application_skill.job_application_skill import JobApplicationSkill
 from app.sql_app.match.match import Match, MatchStatus
 from app.sql_app.professional.professional import ProfessionalStatus
-from app.sql_app.skill.skill import Skill
 
 logger = logging.getLogger(__name__)
 
