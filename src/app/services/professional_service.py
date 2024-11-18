01fb6acf246fea43f6eecc7d2a385e74aca839f1
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-18 22:00:49 +0200

    [FEAT]: Enhance Professional service with update and retrieval functionalities, including matched job ads; Refactor code

--- src/app/services/professional_service.py
+++ src/app/services/professional_service.py
@@ -2,15 +2,19 @@ import logging
 from uuid import UUID
 
 from fastapi import UploadFile, status
-from sqlalchemy import Row
 from sqlalchemy.orm import Session
 
 from app.exceptions.custom_exceptions import ApplicationError
 from app.schemas.common import FilterParams
-from app.schemas.professional import ProfessionalCreate, ProfessionalResponse
+from app.schemas.job_ad import BaseJobAd
+from app.schemas.professional import (
+    ProfessionalCreate,
+    ProfessionalResponse,
+    ProfessionalUpdate,
+)
 from app.schemas.user import UserResponse
 from app.services import city_service
-from app.sql_app.city.city import City
+from app.sql_app.job_application.job_application_status import JobStatus
 from app.sql_app.professional.professional import Professional
 from app.sql_app.professional.professional_status import ProfessionalStatus
 
@@ -29,7 +33,7 @@ def create(
 
     Args:
         user (UserResponse): Current logged in user.
-        professional_create (ProfessionalBase): Pydantic schema for collecting data.
+        professional_create (ProfessionalCreate): Pydantic schema for collecting data.
         professional_status (ProfessionalStatus): The status of the Professional.
         db (Session): Database dependency.
         photo (UploadFile | None): Photo of the professional.
@@ -52,7 +56,6 @@ def create(
     professional = Professional(
         **professional_create.model_dump(exclude={"city"}),
         photo=upload_photo,
-        user_id=user.id,
         city_id=city.id,
         status=professional_status,
     )
@@ -62,13 +65,14 @@ def create(
     db.refresh(professional)
 
     logger.info(f"Professional with id {professional.id} created")
-    return ProfessionalResponse.create(professional=professional, city=city.name)
+    return ProfessionalResponse.create(professional=professional)
 
 
 def update(
     professional_id: UUID,
-    professional_update: ProfessionalCreate,
+    professional_update: ProfessionalUpdate,
     professional_status: ProfessionalStatus,
+    private_matches: bool,
     db: Session,
     photo: UploadFile | None = None,
 ) -> ProfessionalResponse:
@@ -77,7 +81,7 @@ def update(
 
     Args:
         professional_id (UUID): The identifier of the professional.
-        professional_update (ProfessionalBase): Pydantic schema for collecting data.
+        professional_update (ProfessionalUpdate): Pydantic schema for collecting data.
         professional_status (ProfessionalStatus): The status of the Professional.
         db (Session): Database dependency.
         photo (UploadFile | None): Photo of the professional.
@@ -94,37 +98,57 @@ def update(
     """
     professional = _get_by_id(professional_id=professional_id, db=db)
 
-    city = city_service.get_by_name(city_name=professional_update.city, db=db)
-    if professional.city_id != city.id:
-        professional.city_id = city.id
-        logger.info("professional city updated successfully")
-
-    if professional.description != professional_update.description:
-        professional.description = professional_update.description
-        logger.info("Professional description updated successfully")
-
-    if professional.status != professional_status:
-        professional.status = professional_status
-        logger.info("Professional status updated successfully")
-
-    if professional.first_name != professional_update.first_name:
-        professional.first_name = professional_update.first_name
-        logger.info("Professional first name updated successfully")
-
-    if professional.last_name != professional_update.last_name:
-        professional.last_name = professional_update.last_name
-        logger.info("Professional last name updated successfully")
+    professional = _update_atributes(
+        professional_update=professional_update,
+        professional=professional,
+        db=db,
+        professional_status=professional_status,
+        private_matches=private_matches,
+    )
 
     if photo is not None:
-        updload_photo = photo.file.read()
-        professional.photo = updload_photo
+        upload_photos = photo.file.read()
+        professional.photo = upload_photos
         logger.info("Professional photo updated successfully")
+
+    matched_ads = (
+        get_matches(professional=professional, db=db)
+        if not professional.has_private_matches
+        else None
+    )
 
     db.commit()
     db.refresh(professional)
 
     logger.info(f"Professional with id {professional.id} updated successfully")
-    return ProfessionalResponse.create(professional=professional, city=city.name)
+    return ProfessionalResponse.create(
+        professional=professional,
+        matched_ads=matched_ads,
+    )
+
+
+def get_by_id(professional_id: UUID, db: Session) -> ProfessionalResponse:
+    """
+    Retrieve a Professional profile by its ID.
+
+    Args:
+        professional_id (UUID): The identifier of the professional.
+        db (Session): Database session dependency.
+
+    Returns:
+        ProfessionalResponse: The created professional profile response.
+    """
+    professional = _get_by_id(professional_id=professional_id, db=db)
+
+    matched_ads = (
+        get_matches(professional=professional, db=db)
+        if not professional.has_private_matches
+        else None
+    )
+
+    return ProfessionalResponse.create(
+        professional=professional, matched_ads=matched_ads
+    )
 
 
 def get_all(db: Session, filter_params: FilterParams) -> list[ProfessionalResponse]:
@@ -138,23 +162,21 @@ def get_all(db: Session, filter_params: FilterParams) -> list[ProfessionalRespon
         list[ProfessionalResponse]: A list of Professional Profiles that are visible for Companies.
     """
 
-    query = (
-        db.query(Professional, City)
-        .join(City, Professional.city_id == City.id)
-        .filter(Professional.status == ProfessionalStatus.ACTIVE)
+    query = db.query(Professional).filter(
+        Professional.status == ProfessionalStatus.ACTIVE
     )
 
     logger.info("Retreived all professional profiles that are with status ACTIVE")
 
-    result: list[Row[tuple[Professional, City]]] = (
+    result: list[Professional] = (
         query.offset(filter_params.offset).limit(filter_params.limit).all()
     )
 
     logger.info("Limited public topics based on offset and limit")
 
     return [
-        ProfessionalResponse.create(professional=row[0], city=row[1].name)
-        for row in result
+        ProfessionalResponse.create(professional=professional)
+        for professional in result
     ]
 
 
@@ -163,7 +185,7 @@ def _get_by_id(professional_id: UUID, db: Session) -> Professional:
     Retrieves an instance of the Professional model or None.
 
     Args:
-        professional_id (UUID): The identifier.
+        professional_id (UUID): The identifier of the Professional.
         db (Session): Database dependency.
 
     Returns:
@@ -187,18 +209,90 @@ def _get_by_id(professional_id: UUID, db: Session) -> Professional:
     return professional
 
 
-def get_by_id(professional_id: UUID, db: Session) -> ProfessionalResponse:
+def get_matches(professional: Professional, db: Session) -> list[BaseJobAd]:
     """
-    Retrieve a Professional profile by its ID.
+    Fetches Matched Job Ads for the given Professional.
 
     Args:
-        professional_id (UUID): The identifier of the professional.
-        db (Session): Database session dependency.
+        professional (Professional): The existing professional object to be updated.
+        private_matches (bool): Indicates if the professional has private matches.
+        db (Session): Database dependency.
 
     Returns:
-        ProfessionalResponse: The created professional profile response.
+        list[BaseJobAd]: List of Pydantic models containing basic information about the matched Job Ad.
     """
-    professional = _get_by_id(professional_id=professional_id, db=db)
-    city = city_service.get_by_id(city_id=professional.city_id, db=db)
+    ads = [
+        match.job_ad
+        for job_application in professional.job_applications
+        if job_application.status == JobStatus.MATCHED
+        for match in job_application.matches
+    ]
 
-    return ProfessionalResponse.create(professional=professional, city=city.name)
+    return [
+        BaseJobAd(
+            title=ad.title,
+            description=ad.description,
+            location=ad.location.name,
+            min_salary=ad.min_salary,
+            max_salary=ad.max_salary,
+        )
+        for ad in ads
+    ]
+
+
+def _update_atributes(
+    professional_update: ProfessionalUpdate,
+    professional: Professional,
+    private_matches: bool,
+    db: Session,
+    professional_status: ProfessionalStatus,
+) -> Professional:
+    """
+    Updates the attributes of a professional's profile based on the provided ProfessionalUpdate model.
+
+    Args:
+        professional_update (ProfessionalUpdate): The updated information for the professional.
+        professional (Professional): The existing professional object to be updated.
+        private_matches (bool): Indicates if the professional has private matches.
+        db (Session): The database session used for querying or interacting with the database.
+        professional_status (ProfessionalStatus): The new professional status to be applied if different.
+
+    Returns:
+        Professional: The updated professional object with modified attributes.
+    """
+    if professional.status != professional_status:
+        professional.status = professional_status
+        logger.info("Professional status updated successfully")
+
+    if (
+        professional_update.city is not None
+        and professional_update.city != professional.city.name
+    ):
+        city = city_service.get_by_name(city_name=professional_update.city, db=db)
+        professional.city_id = city.id
+        logger.info("professional city updated successfully")
+
+    if (
+        professional_update.description is not None
+        and professional.description != professional_update.description
+    ):
+        professional.description = professional_update.description
+        logger.info("Professional description updated successfully")
+
+    if (
+        professional_update.first_name
+        and professional.first_name != professional_update.first_name
+    ):
+        professional.first_name = professional_update.first_name
+        logger.info("Professional first name updated successfully")
+
+    if (
+        professional_update.last_name
+        and professional.last_name != professional_update.last_name
+    ):
+        professional.last_name = professional_update.last_name
+        logger.info("Professional last name updated successfully")
+
+    professional.has_private_matches = private_matches
+
+    return professional
