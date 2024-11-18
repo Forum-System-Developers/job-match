27c23f084e4d05ab7f9f273e2717712c9c9ac322
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-18 13:34:20 +0200

    [FIX]: Update JobAdsRequirement model to replace JobApplication with JobRequirement.

--- src/app/sql_app/job_ad_requirement/job_ads_requirement.py
+++ src/app/sql_app/job_ad_requirement/job_ads_requirement.py
@@ -10,22 +10,22 @@ from app.sql_app.database import Base
 
 if TYPE_CHECKING:
     from app.sql_app.job_ad.job_ad import JobAd
-    from app.sql_app.job_application.job_application import JobApplication
+    from app.sql_app.job_requirement.job_requirement import JobRequirement
 
 
 class JobAdsRequirement(Base):
     """
-    JobAdsRequirement is a SQLAlchemy model representing the requirements for job advertisements.
+    Represents the requirements for a job advertisement.
 
     Attributes:
-        job_ad_id (UUID): Foreign key referencing the ID of the job advertisement.
-        job_application_id (UUID): Foreign key referencing the ID of the job application.
-        created_at (DateTime): Timestamp when the record was created, with timezone support.
-        updated_at (DateTime): Timestamp when the record was last updated, with timezone support.
+        job_ad_id (uuid.UUID): The unique identifier of the job advertisement.
+        job_application_id (uuid.UUID): The unique identifier of the job application.
+        created_at (datetime): The timestamp when the requirement was created.
+        updated_at (datetime): The timestamp when the requirement was last updated.
 
     Relationships:
-        job_ad (relationship): Relationship to the JobAd model.
-        job_application (relationship): Relationship to the JobApplication model.
+        job_ad (JobAd): The relationship to the JobAd model.
+        job_requirement (JobRequirement): The relationship to the JobRequirement model.
     """
 
     __tablename__ = "job_ads_requirement"
@@ -46,6 +46,6 @@ class JobAdsRequirement(Base):
     job_ad: Mapped["JobAd"] = relationship(
         "JobAd", back_populates="job_ads_requirements"
     )
-    job_application: Mapped["JobApplication"] = relationship(
-        "JobApplication", back_populates="job_ads_requirements"
+    job_requirement: Mapped["JobRequirement"] = relationship(
+        "JobRequirement", back_populates="job_ads_requirements"
     )
