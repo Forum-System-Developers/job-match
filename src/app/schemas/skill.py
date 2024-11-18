d16e6bc2bb8c8b00f93ee175b64047e93e4019fc
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-18 02:05:17 +0200

    [FEAT] Add SkillBase Pydantic model

---
+++ src/app/schemas/skill.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from app.sql_app.job_requirement.skill_level import SkillLevel
+
+
+class SkillBase(BaseModel):
+    """
+    Pydantic model representing a skill associated with a specific skill level.
+
+    Attributes:
+        name (str): Skill name.
+        level (SkillLevel): Enum representing the level of proficiency for the skill.
+
+    """
+
+    name: str
+    level: SkillLevel
+
+    class Config:
+        use_enum_values = True
