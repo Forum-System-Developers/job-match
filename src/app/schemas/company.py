1fbe78483b1ba4784eb2ff97fe4c7e563fcda1aa
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-19 18:13:09 +0200

    [REFACTOR]: Update Company schema to use city_id and add create method for instantiation.

--- src/app/schemas/company.py
+++ src/app/schemas/company.py
@@ -1,23 +1,38 @@
 from uuid import UUID
 
-from pydantic import BaseModel, EmailStr, PositiveInt
+from pydantic import BaseModel, EmailStr
 
 from app.schemas.custom_types import Password, Username
+from app.sql_app.company.company import Company
 
 
 class CompanyBase(BaseModel):
     id: UUID
     name: str
     address_line: str
-    city: str
+    city_id: UUID
     description: str
     email: EmailStr
     phone_number: str
-    active_job_ads: PositiveInt
-    successful_matches: PositiveInt
+    active_job_ads: int = 0
+    successful_matches: int = 0
 
     class Config:
         from_attribute = True
+
+    @classmethod
+    def create(cls, company: Company):
+        return cls(
+            id=company.id,
+            name=company.name,
+            address_line=company.address_line,
+            city_id=company.city_id,
+            description=company.description,
+            email=company.email,
+            phone_number=company.phone_number,
+            active_job_ads=company.active_job_count or 0,
+            successful_matches=company.successfull_matches_count or 0,
+        )
 
 
 class CompanyCreate(BaseModel):
@@ -25,7 +40,7 @@ class CompanyCreate(BaseModel):
     password: Password  # type: ignore
     name: str
     address_line: str
-    city: str
+    city_id: UUID
     description: str
     email: EmailStr
     phone_number: str
