6deae48ae42f5445b611a39b73cde9f769b1b588
Sph1nx <nikita.ryapok@proton.me>
2024-11-19 13:31:47 +0200

    [DOCS]: Add docstrings

--- src/app/services/auth_service.py
+++ src/app/services/auth_service.py
@@ -21,6 +21,15 @@ logger = logging.getLogger(__name__)
 
 
 def login(login_data: UserLogin, db: Session) -> dict:
+    """
+    Authenticates a user based on their role and generates access and refresh tokens.
+
+    Args:
+        login_data (UserLogin): The login data containing user credentials and role.
+        db (Session): The database session for querying user information.
+    Returns:
+        dict: A dictionary containing the access and refresh tokens.
+    """
     if login_data.role == "company":
         company = authenticate_user(login_data=login_data, db=db)
         logger.info(f"Authenticated company {company.id}")
@@ -44,6 +53,14 @@ def login(login_data: UserLogin, db: Session) -> dict:
 
 
 def _create_access_token(data: dict) -> str:
+    """
+    Generates an access token for the given user.
+
+    Args:
+        data (dict): A dictionary containing user information.
+    Returns:
+        str: The generated access token as a string.
+    """
     access_token = _create_token(
         data=data,
         expires_delta=timedelta(minutes=get_settings().ACCESS_TOKEN_EXPIRE_MINUTES),
@@ -54,6 +71,14 @@ def _create_access_token(data: dict) -> str:
 
 
 def _create_refresh_token(data: dict) -> str:
+    """
+    Generates a refresh token for the given user data.
+
+    Args:
+        data (dict): A dictionary containing user information.
+    Returns:
+        str: The generated refresh token.
+    """
     refresh_token = _create_token(
         data=data,
         expires_delta=timedelta(days=get_settings().REFRESH_TOKEN_EXPIRE_DAYS),
@@ -64,6 +89,16 @@ def _create_refresh_token(data: dict) -> str:
 
 
 def _create_token(data: dict, expires_delta: timedelta) -> str:
+    """
+    Create JWT with the given data.
+
+    Args:
+        data (dict): The payload data to include in the token.
+        expires_delta (timedelta): The time duration after which the token will expire.
+
+    Returns:
+        str: The encoded JWT as a string.
+    """
     try:
         payload = data.copy()
         expire = datetime.now() + expires_delta
@@ -81,6 +116,15 @@ def _create_token(data: dict, expires_delta: timedelta) -> str:
 
 
 def create_access_and_refresh_tokens(user: User, login_data: UserLogin) -> dict:
+    """
+    Generate access and refresh tokens for a given user.
+
+    Args:
+        user (User): The user object containing user details.
+        login_data (UserLogin): The login data containing user role information.
+    Returns:
+        dict: A dictionary containing the access token, refresh token, and token type.
+    """
     token_data = {"sub": str(user.id), "role": login_data.role}
     logger.info(f"Created token data for user {user.id}")
     access_token = _create_access_token(token_data)
@@ -96,6 +140,15 @@ def create_access_and_refresh_tokens(user: User, login_data: UserLogin) -> dict:
 
 
 def verify_token(token: str, db: Session) -> dict:
+    """
+    Verifies the provided JWT and retrieves the associated user information.
+
+    Args:
+        token (str): The JWT token to be verified.
+        db (Session): The database session to use for querying user information.
+    Returns:
+        dict: The decoded token payload if verification is successful.
+    """
     try:
         payload = jwt.decode(
             token, get_settings().SECRET_KEY, algorithms=[get_settings().ALGORITHM]
@@ -130,6 +183,15 @@ def verify_token(token: str, db: Session) -> dict:
 
 
 def authenticate_user(login_data: UserLogin, db: Session) -> User:
+    """
+    Authenticates a user based on their role and login credentials.
+
+    Args:
+        login_data (UserLogin): The login data containing user credentials.
+        db (Session): The database session used to query the user information.
+    Returns:
+        User: The authenticated user object.
+    """
     user: User
     verified_password: bool = False
 
@@ -155,6 +217,16 @@ def authenticate_user(login_data: UserLogin, db: Session) -> User:
 
 
 def refresh_access_token(refresh_token: str, db: Session) -> str:
+    """
+    Refreshes the access token using the provided refresh token.
+
+    Args:
+        refresh_token (str): The refresh token to be verified and used for generating a new access token.
+        db (Session): The database session used for verifying the token.
+    Returns:
+        str: The newly created access token.
+    Raises:
+    """
     payload = verify_token(token=refresh_token, db=db)
     user_id = payload.get("sub")
     user_role = payload.get("role")
@@ -168,6 +240,15 @@ def refresh_access_token(refresh_token: str, db: Session) -> str:
 def get_current_company(
     token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
 ) -> CompanyResponse:
+    """
+    Retrieve the current company based on the provided token.
+
+    Args:
+        token (str): The authentication token provided by the user.
+        db (Session): The database session dependency.
+    Returns:
+        CompanyResponse: The company information associated with the current user.
+    """
     token_data = verify_token(token=token, db=db)
     user_id = UUID(token_data.get("sub"))
     company = company_service.get_by_id(id=user_id, db=db)
@@ -179,6 +260,15 @@ def get_current_company(
 def get_current_professional(
     token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
 ) -> ProfessionalResponse:
+    """
+    Retrieve the current professional user based on the provided token.
+
+    Args:
+        token (str): The OAuth2 token used for authentication.
+        db (Session): The database session dependency.
+    Returns:
+        ProfessionalResponse: The professional user information.
+    """
     token_data = verify_token(token=token, db=db)
     user_id = UUID(token_data.get("sub"))
     professional = professional_service.get_by_id(professional_id=user_id, db=db)
