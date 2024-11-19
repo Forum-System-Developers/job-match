8ea3180462afa5ea0534e93c83c38624719b6631
Sph1nx <nikita.ryapok@proton.me>
2024-11-19 13:20:46 +0200

    [REFACTOR]: Refactor authentication service

--- src/app/services/auth_service.py
+++ src/app/services/auth_service.py
@@ -9,26 +9,41 @@ from sqlalchemy.orm import Session
 
 from app.core.config import get_settings
 from app.exceptions.custom_exceptions import ApplicationError
-from app.schemas.token import Token, TokenData
-from app.schemas.user import UserResponse
-from app.services import user_service
+from app.schemas.company import CompanyResponse
+from app.schemas.professional import ProfessionalResponse
+from app.schemas.user import User, UserLogin
+from app.services import company_service, professional_service
 from app.sql_app.database import get_db
-from app.sql_app.user.user import User
 from app.utils.password_utils import verify_password
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="")
 logger = logging.getLogger(__name__)
 
 
-def _create_access_token(data: dict) -> str:
-    """
-    Generates an access token for the given user.
-    Args:
-        data (dict): A dictionary containing user information.
-    Returns:
-        str: The generated access token as a string.
-    """
+def login(login_data: UserLogin, db: Session) -> dict:
+    if login_data.role == "company":
+        company = authenticate_user(login_data=login_data, db=db)
+        logger.info(f"Authenticated company {company.id}")
+        token = create_access_and_refresh_tokens(user=company, login_data=login_data)
+        logger.info(f"Created tokens for company {company.id}")
+    elif login_data.role == "professional":
+        professional = authenticate_user(login_data=login_data, db=db)
+        logger.info(f"Authenticated professional {professional.id}")
+        token = create_access_and_refresh_tokens(
+            user=professional, login_data=login_data
+        )
+        logger.info(f"Created tokens for professional {professional.id}")
+    else:
+        logger.error(f"Invalid role {login_data.role}")
+        raise ApplicationError(
+            detail="Invalid role",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
 
+    return token
+
+
+def _create_access_token(data: dict) -> str:
     access_token = _create_token(
         data=data,
         expires_delta=timedelta(minutes=get_settings().ACCESS_TOKEN_EXPIRE_MINUTES),
@@ -39,14 +54,6 @@ def _create_access_token(data: dict) -> str:
 
 
 def _create_refresh_token(data: dict) -> str:
-    """
-    Generates a refresh token for the given user data.
-    Args:
-        data (dict): A dictionary containing user information.
-    Returns:
-        str: The generated refresh token as a string.
-    """
-
     refresh_token = _create_token(
         data=data,
         expires_delta=timedelta(days=get_settings().REFRESH_TOKEN_EXPIRE_DAYS),
@@ -57,17 +64,6 @@ def _create_refresh_token(data: dict) -> str:
 
 
 def _create_token(data: dict, expires_delta: timedelta) -> str:
-    """
-    Creates a JSON Web Token (JWT) with the given data and expiration time.
-    Args:
-        data (dict): The payload data to include in the token.
-        expires_delta (timedelta): The time duration after which the token will expire.
-    Returns:
-        str: The encoded JWT as a string.
-    Raises:
-        ApplicationError: If the token cannot be created.
-    """
-
     try:
         payload = data.copy()
         expire = datetime.now() + expires_delta
@@ -84,39 +80,22 @@ def _create_token(data: dict, expires_delta: timedelta) -> str:
         )
 
 
-def create_access_and_refresh_tokens(user: User) -> Token:
-    """
-    Create access and refresh tokens for a given user.
-    Args:
-        user (UserResponse): The user for whom the tokens are being created.
-    Returns:
-        Token: An object containing the access token, refresh token, and token type.
-    """
-
-    token_data = {"sub": str(user.id)}
+def create_access_and_refresh_tokens(user: User, login_data: UserLogin) -> dict:
+    token_data = {"sub": str(user.id), "role": login_data.role}
     logger.info(f"Created token data for user {user.id}")
     access_token = _create_access_token(token_data)
     logger.info(f"Created access token for user {user.id}")
     refresh_token = _create_refresh_token(token_data)
     logger.info(f"Created refresh token for user {user.id}")
 
-    return Token(
-        access_token=access_token, refresh_token=refresh_token, token_type="bearer"
-    )
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
 
 
 def verify_token(token: str, db: Session) -> dict:
-    """
-    Verifies a JWT token and retrieves the associated user from the database.
-    Args:
-        token (str): The JWT token to be verified.
-        db (Session): The database session to use for retrieving the user.
-    Returns:
-        dict: The decoded payload of the JWT token.
-    Raises:
-        ApplicationError: If the token cannot be verified or decoded.
-    """
-
     try:
         payload = jwt.decode(
             token, get_settings().SECRET_KEY, algorithms=[get_settings().ALGORITHM]
@@ -128,60 +107,81 @@ def verify_token(token: str, db: Session) -> dict:
             detail="Could not verify token", status_code=status.HTTP_401_UNAUTHORIZED
         )
 
-    user_id = UUID(payload.get("sub"))
-    user_service.get_by_id(id=user_id, db=db)
-
-    logger.info(f"Retrieved user {user_id}")
+    if payload.get("role") == "company":
+        company = company_service.get_by_id(id=UUID(payload.get("sub")), db=db)
+        if not company:
+            logger.error(f"Company {payload.get('sub')} not found")
+            raise ApplicationError(
+                detail="Company not found",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            )
+    elif payload.get("role") == "professional":
+        professional = professional_service.get_by_id(
+            professional_id=UUID(payload.get("sub")), db=db
+        )
+        if not professional:
+            logger.error(f"Professional {payload.get('sub')} not found")
+            raise ApplicationError(
+                detail="Professional not found",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            )
 
     return payload
 
 
-def authenticate_user(username: str, password: str, db: Session) -> UserResponse:
-    """
-    Authenticate a user by their username and password.
-    Args:
-        username (str): The username of the user.
-        password (str): The password of the user.
-        db (Session): The database session to use for querying the user.
-    Returns:
-        User: The authenticated user object.
-    Raises:
-        ApplicationError: If the user cannot be authenticated due to invalid credentials.
-    """
+def authenticate_user(login_data: UserLogin, db: Session) -> User:
+    user: User
+    verified_password: bool = False
 
-    user = user_service.get_by_username(username=username, db=db)
-    logger.info(f"Retrieved user {user.id} with username {username}")
-
-    verified_password = verify_password(password, user.password)
+    if login_data.role == "company":
+        user = company_service.get_by_username(username=login_data.username, db=db)
+        logger.info(f"Retrieved company {user.id}")
+        verified_password = verify_password(login_data.password, user.password)
+        logger.info(f"Password verified for company {user.id}")
+    elif login_data.role == "professional":
+        user = professional_service.get_by_username(username=login_data.username, db=db)
+        logger.info(f"Retrieved professional {user.id}")
+        verified_password = verify_password(login_data.password, user.password)
+        logger.info(f"Password verified for professional {user.id}")
 
     if not verified_password:
-        logger.error(f"Invalid password for user {user.id}")
+        logger.error("Invalid password")
         raise ApplicationError(
             detail="Could not authenticate user",
             status_code=status.HTTP_401_UNAUTHORIZED,
         )
-    logger.info(f"Password verified for user {user.id}")
 
     return user
 
 
-def get_current_user(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
-) -> UserResponse:
-    """
-    Retrieve the current user based on the provided token.
-    Args:
-        token (str): The authentication token provided by the user.
-        db (Session): The database session dependency.
-    Returns:
-        UserResponse: The user information retrieved from the database.
-    Raises:
-        ApplicationError: If user not found.
-    """
+def refresh_access_token(refresh_token: str, db: Session) -> str:
+    payload = verify_token(token=refresh_token, db=db)
+    user_id = payload.get("sub")
+    user_role = payload.get("role")
+    logger.info(f"Verified refresh token for user {user_id}")
+    access_token = _create_access_token({"sub": user_id, "role": user_role})
+    logger.info(f"Created new access token for user {user_id}")
 
+    return access_token
+
+
+def get_current_company(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+) -> CompanyResponse:
     token_data = verify_token(token=token, db=db)
     user_id = UUID(token_data.get("sub"))
-    user = user_service.get_by_id(id=user_id, db=db)
+    company = company_service.get_by_id(id=user_id, db=db)
     logger.info(f"Retrieved current user {user_id}")
 
-    return user
+    return company
+
+
+def get_current_professional(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+) -> ProfessionalResponse:
+    token_data = verify_token(token=token, db=db)
+    user_id = UUID(token_data.get("sub"))
+    professional = professional_service.get_by_id(professional_id=user_id, db=db)
+    logger.info(f"Retrieved current user {user_id}")
+
+    return professional
