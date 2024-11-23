408499563c638fa7989ea6a42016cdf8ec58d59d
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-23 22:39:05 +0200

    [REFACTOR]: Update test data with additional constants.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,16 @@
 import uuid
 
-VALID_JOB_AD_ID = uuid.uuid4()
 VALID_COMPANY_ID = uuid.uuid4()
+VALID_COMPANY_USERNAME = "test_username"
+
+VALID_PASSWORD = "test_password"
+
+VALID_JOB_AD_ID = uuid.uuid4()
 VALID_PROFESSIONAL_ID = uuid.uuid4()
 VALID_JOB_APPLICATION_ID = uuid.uuid4()
 VALID_REQUIREMENT_ID = uuid.uuid4()
-NON_EXISTENT_ID = uuid.uuid4()
 
 VALID_CITY_NAME = "Test City"
+
+NON_EXISTENT_ID = uuid.uuid4()
+NON_EXISTENT_USERNAME = "non_existent_username"
