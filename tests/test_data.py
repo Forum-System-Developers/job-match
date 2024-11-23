a95cb4801fe2f22f9933c420f85b0469cf242924
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-23 23:53:19 +0200

    [FEAT]: Add constants to test data.

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,12 +4,14 @@ VALID_COMPANY_ID = uuid.uuid4()
 VALID_COMPANY_USERNAME = "test_username"
 
 VALID_PASSWORD = "test_password"
+HASHED_PASSWORD = "hashed_password"
 
 VALID_JOB_AD_ID = uuid.uuid4()
 VALID_PROFESSIONAL_ID = uuid.uuid4()
 VALID_JOB_APPLICATION_ID = uuid.uuid4()
 VALID_REQUIREMENT_ID = uuid.uuid4()
 
+VALID_CITY_ID = uuid.uuid4()
 VALID_CITY_NAME = "Test City"
 
 NON_EXISTENT_ID = uuid.uuid4()
