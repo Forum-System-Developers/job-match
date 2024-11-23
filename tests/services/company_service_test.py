6ab0dbcb746300a408fa74a73c6a6e01033e07c0
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-23 22:40:48 +0200

    [TEST]: Add unit tests for get_all function in company_service.

---
+++ tests/services/company_service_test.py
@@ -0,0 +1,46 @@
+import pytest
+from fastapi import status
+
+from app.exceptions.custom_exceptions import ApplicationError
+from app.services import company_service
+from app.sql_app.company.company import Company
+from tests import test_data as td
+from tests.utils import assert_filter_called_with
+
+
+@pytest.fixture
+def mock_db(mocker):
+    return mocker.Mock()
+
+
+def test_getAll_returnsCompanies_whenCompaniesAreFound(
+    mocker,
+    mock_db,
+) -> None:
+    # Arrange
+    mock_filter_params = mocker.Mock(offset=0, limit=10)
+    mock_companies = [mocker.Mock(), mocker.Mock()]
+    mock_company_response = [mocker.Mock(), mocker.Mock()]
+
+    mock_query = mock_db.query.return_value
+    mock_offset = mock_query.offset.return_value
+    mock_limit = mock_offset.limit.return_value
+    mock_limit.all.return_value = mock_companies
+
+    mock_create = mocker.patch(
+        "app.schemas.company.CompanyResponse.create",
+        side_effect=mock_company_response,
+    )
+
+    # Act
+    result = company_service.get_all(filter_params=mock_filter_params, db=mock_db)
+
+    # Assert
+    mock_db.query.assert_called_with(Company)
+    mock_query.offset.assert_called_with(mock_filter_params.offset)
+    mock_offset.limit.assert_called_with(mock_filter_params.limit)
+    mock_create.assert_any_call(mock_companies[0])
+    mock_create.assert_any_call(mock_companies[1])
+    assert len(result) == 2
+    assert result[0] == mock_company_response[0]
+    assert result[1] == mock_company_response[1]
