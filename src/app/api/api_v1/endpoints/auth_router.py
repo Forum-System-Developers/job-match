d1a4798b5cb831b151f77d714c42496bfc05889a
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-23 16:12:42 +0200

    [FIX]: Fix authentication endpoints response types and dependencies

--- src/app/api/api_v1/endpoints/auth_router.py
+++ src/app/api/api_v1/endpoints/auth_router.py
@@ -1,12 +1,10 @@
-from fastapi import APIRouter, Depends, status
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from app.schemas.token import AccessToken, Token
-from app.schemas.user import UserLogin
-from app.services.auth_service import login, refresh_access_token
+from app.schemas.token import Token
+from app.services.auth_service import login, oauth2_scheme, refresh_access_token
 from app.sql_app.database import get_db
-from app.utils.process_request import process_request
 
 router = APIRouter()
 
@@ -15,27 +13,17 @@ router = APIRouter()
     "/login",
     description="Login a user.",
 )
-def login_user(login_data: UserLogin, db: Session = Depends(get_db)) -> JSONResponse:
-    def _login() -> Token:
-        return login(login_data=login_data, db=db)
-
-    return process_request(
-        get_entities_fn=_login,
-        status_code=status.HTTP_200_OK,
-        not_found_err_msg="Login failed",
-    )
+def login_user(
+    login_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+) -> Token:
+    return login(username=login_data.username, password=login_data.password, db=db)
 
 
 @router.post(
     "/refresh",
     description="Refresh the access token.",
 )
-def refresh_token(refresh_token: str, db: Session = Depends(get_db)) -> JSONResponse:
-    def _refresh() -> AccessToken:
-        return refresh_access_token(refresh_token=refresh_token, db=db)
-
-    return process_request(
-        get_entities_fn=_refresh,
-        status_code=status.HTTP_200_OK,
-        not_found_err_msg="Token refresh failed",
-    )
+def refresh_token(
+    refresh_token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+) -> Token:
+    return refresh_access_token(refresh_token=refresh_token, db=db)
