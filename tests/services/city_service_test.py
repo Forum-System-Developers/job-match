9ade6d1c623f79cf9a581a254ee0416fc0f577d6
Sph1nx <nikita.ryapok@proton.me>
2024-11-24 18:10:30 +0200

    [FIX]: Refactor test to use get_id for city ID retrieval instead of get_by_name

--- tests/services/city_service_test.py
+++ tests/services/city_service_test.py
@@ -5,7 +5,7 @@ from fastapi import status
 
 from app.exceptions.custom_exceptions import ApplicationError
 from app.schemas.address import CityResponse
-from app.services.city_service import get_by_id, get_by_name
+from app.services.city_service import get_by_id, get_by_name, get_id
 from app.sql_app.city.city import City
 from tests import test_data as td
 from tests.utils import assert_filter_called_with
@@ -105,13 +105,13 @@ def test_getId_returns_city_id_when_city_is_found(mocker, mock_db):
     mock_filter.first.return_value = city
 
     # Act
-    result = get_by_name(city_name=td.VALID_CITY_NAME, db=mock_db)
+    result = get_id(db=mock_db, city_name=td.VALID_CITY_NAME)
 
     # Assert
     mock_db.query.assert_called_once_with(City)
     mock_query.filter.assert_called_once_with(ANY)
     assert_filter_called_with(mock_query, City.name == td.VALID_CITY_NAME)
-    assert result.id == td.VALID_CITY_ID
+    assert result == td.VALID_CITY_ID
 
 
 def test_getId_raises_error_when_city_is_not_found(mock_db):
