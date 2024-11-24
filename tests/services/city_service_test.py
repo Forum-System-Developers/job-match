d1ca8ac6710a9c7d417c07a81e7f5c21db6065e9
Sph1nx <nikita.ryapok@proton.me>
2024-11-24 17:40:37 +0200

    [TEST]: Add unit test for get_by_id

--- tests/services/city_service_test.py
+++ tests/services/city_service_test.py
@@ -53,3 +53,25 @@ def test_getByName_raises_error_when_city_is_not_found(mock_db):
     assert_filter_called_with(mock_query, City.name == td.VALID_CITY_NAME)
     assert exc.value.data.status == status.HTTP_404_NOT_FOUND
     assert exc.value.data.detail == f"City with name {td.VALID_CITY_NAME} was not found"
+
+
+def test_getById_returns_city_when_city_is_found(mocker, mock_db):
+    # Arrange
+    city = mocker.Mock()
+    city.id = td.VALID_CITY_ID
+    city.name = td.VALID_CITY_NAME
+
+    mock_query = mock_db.query.return_value
+    mock_filter = mock_query.filter.return_value
+    mock_filter.first.return_value = city
+
+    expected_city_response = CityResponse(id=td.VALID_CITY_ID, name=td.VALID_CITY_NAME)
+
+    # Act
+    result = get_by_name(city_name=td.VALID_CITY_NAME, db=mock_db)
+
+    # Assert
+    mock_db.query.assert_called_once_with(City)
+    mock_query.filter.assert_called_once_with(ANY)
+    assert_filter_called_with(mock_query, City.name == td.VALID_CITY_NAME)
+    assert result == expected_city_response
