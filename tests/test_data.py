b32e0e7343840192cd8a3c7567490581d1747a9e
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-24 13:18:02 +0200

    [REFACTOR]: Add more test data for improved testing coverage.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,15 @@
 import uuid
 
 VALID_COMPANY_ID = uuid.uuid4()
+VALID_COMPANY_NAME = "Test Company"
 VALID_COMPANY_USERNAME = "test_username"
+VALID_COMPANY_DESCRIPTION = "Test Description"
+VALID_COMPANY_ADDRESS_LINE = "Test Address Line"
+VALID_COMPANY_EMAIL = "test_company_email@example.com"
+VALID_COMPANY_PHONE_NUMBER = "1234567890"
+
+VALID_COMPANY_NAME_2 = "Test Company 2"
+VALID_COMPANY_DESCRIPTION_2 = "Test Description 2"
 
 VALID_PASSWORD = "test_password"
 HASHED_PASSWORD = "hashed_password"
@@ -16,3 +24,13 @@ VALID_CITY_NAME = "Test City"
 
 NON_EXISTENT_ID = uuid.uuid4()
 NON_EXISTENT_USERNAME = "non_existent_username"
+
+COMPANY = {
+    "id": VALID_COMPANY_ID,
+    "name": VALID_COMPANY_NAME, 
+    "description": VALID_COMPANY_DESCRIPTION, 
+    "address_line": VALID_COMPANY_ADDRESS_LINE, 
+    "city": VALID_CITY_NAME, 
+    "email": VALID_COMPANY_EMAIL,
+    "phone_number": VALID_COMPANY_PHONE_NUMBER,
+}
