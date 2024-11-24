826124ad8c53c288204f0a589f7047ad02f618ed
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-24 18:38:05 +0200

    [REFACTOR]: move process_async_request to processors and remove process_request.

--- src/app/utils/processors.py
+++ src/app/utils/processors.py
@@ -2,7 +2,7 @@ import logging
 from typing import Any, Callable, Union
 
 from fastapi import status
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 from pydantic import BaseModel
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.orm import Session
@@ -36,6 +36,56 @@ def process_request(
     try:
         response = get_entities_fn()
         return JSONResponse(status_code=status_code, content=_format_response(response))
+    except ApplicationError as ex:
+        logger.exception(str(ex))
+        return JSONResponse(
+            status_code=ex.data.status,
+            content={"detail": {"error": ex.data.detail}},
+        )
+    except TypeError as ex:
+        logger.exception(not_found_err_msg)
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"detail": {"error": str(ex)}},
+        )
+    except SyntaxError as ex:
+        logger.exception("Pers thrown an exception")
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"detail": {"error": str(ex)}},
+        )
+
+
+async def process_async_request(
+    get_entities_fn: Callable,
+    status_code: int,
+    not_found_err_msg: str,
+) -> JSONResponse | RedirectResponse:
+    """
+    Asynchronously processes a request by calling the provided function to get entities and returns an appropriate response.
+
+    Args:
+        get_entities_fn (Callable): A function that retrieves entities asynchronously.
+        status_code (int): The HTTP status code to return in the response if successful.
+        not_found_err_msg (str): The error message to log if a TypeError occurs.
+
+    Returns:
+        JSONResponse | RedirectResponse: A JSON response with the formatted data or a redirect response.
+
+    Raises:
+        ApplicationError: If an application-specific error occurs.
+        TypeError: If a type error occurs, typically indicating a not found error.
+        SyntaxError: If a syntax error occurs, typically indicating a bad request.
+    """
+    try:
+        response = await get_entities_fn()
+
+        if isinstance(response, RedirectResponse):
+            return response
+
+        formatted_response = _format_response(response)
+
+        return JSONResponse(status_code=status_code, content=formatted_response)
     except ApplicationError as ex:
         logger.exception(str(ex))
         return JSONResponse(
