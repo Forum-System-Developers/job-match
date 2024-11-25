a946f29fe4f2ce6b8cc368db4482efd8745fccfc
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-25 19:11:08 +0200

    [TEST]: Add unit test for updating job advertisement maximum salary when provided.

--- tests/services/job_ad_service_test.py
+++ tests/services/job_ad_service_test.py
@@ -342,3 +342,31 @@ def test_updateJobAd_updatesMinSalary_whenMinSalaryIsProvided(mocker, mock_db) -
     assert result.location == mock_job_ad.location
     assert result.max_salary == mock_job_ad.max_salary
     assert result.status == mock_job_ad.status
+
+
+def test_updateJobAd_updatesMaxSalary_whenMaxSalaryIsProvided(mocker, mock_db) -> None:
+    # Arrange
+    mock_job_ad = mocker.Mock(**td.JOB_AD)
+    job_ad_update_data = JobAdUpdate(max_salary=2000.00)
+
+    mock_ensure_valid_city = mocker.patch(
+        "app.services.job_ad_service.ensure_valid_city"
+    )
+
+    # Act
+    result = _update_job_ad(
+        job_ad_data=job_ad_update_data,
+        job_ad=mock_job_ad,
+        db=mock_db,
+    )
+
+    # Assert
+    mock_ensure_valid_city.assert_not_called()
+    assert result.max_salary == job_ad_update_data.max_salary
+    assert isinstance(result.updated_at, datetime)
+
+    assert result.title == mock_job_ad.title
+    assert result.description == mock_job_ad.description
+    assert result.location == mock_job_ad.location
+    assert result.min_salary == mock_job_ad.min_salary
+    assert result.status == mock_job_ad.status
