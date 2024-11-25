4f9c17e13123772829c816bd304c4ae1763439ec
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-25 11:04:12 +0200

    [TEST]: Add unit test for job advertisement retrieval when no job ads exist.

--- tests/services/job_ad_service_test.py
+++ tests/services/job_ad_service_test.py
@@ -43,3 +43,27 @@ def test_getAll_returnsJobAds_whenJobAdsExist(mocker, mock_db) -> None:
     assert len(result) == 2
     assert result[0] == job_ad_responses[0]
     assert result[1] == job_ad_responses[1]
+
+
+def test_getAll_returnsEmptyList_whenNoJobAdsExist(mocker, mock_db) -> None:
+    # Arrange
+    filter_params = FilterParams(offset=0, limit=10)
+    search_params = JobAdSearchParams()
+
+    mock_query = mock_db.query.return_value
+    mock_offset = mock_query.offset.return_value
+    mock_limit = mock_offset.limit.return_value
+    mock_limit.all.return_value = []
+
+    mock_search_job_ads = mocker.patch(
+        "app.services.job_ad_service._search_job_ads", return_value=mock_query
+    )
+
+    # Act
+    result = get_all(filter_params, search_params, mock_db)
+
+    # Assert
+    mock_search_job_ads.assert_called_with(search_params=search_params, db=mock_db)
+    mock_query.offset.assert_called_with(filter_params.offset)
+    mock_offset.limit.assert_called_with(filter_params.limit)
+    assert result == []
