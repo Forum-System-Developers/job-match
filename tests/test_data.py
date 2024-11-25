603e31ffbe7e7dff7ad26d94a96c81f47c48cc90
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-25 11:01:26 +0200

    [TEST]: Add job advertisement test data for validation.

--- tests/test_data.py
+++ tests/test_data.py
@@ -15,12 +15,20 @@ VALID_COMPANY_ADDRESS_LINE_2 = "Test Address Line 2"
 VALID_COMPANY_EMAIL_2 = "test_company_email2@example.com"
 VALID_COMPANY_PHONE_NUMBER_2 = "0987654321"
 
-VALID_PASSWORD = "test_password"
-HASHED_PASSWORD = "hashed_password"
-
 VALID_JOB_AD_ID = uuid.uuid4()
-VALID_PROFESSIONAL_ID = uuid.uuid4()
+VALID_JOB_AD_DESCRIPTION = "Test Description"
+VALID_JOB_AD_TITLE = "Test Job Ad"
+
+VALID_JOB_AD_ID_2 = uuid.uuid4()
+VALID_JOB_AD_DESCRIPTION_2 = "Test Description 2"
+VALID_JOB_AD_TITLE_2 = "Test Job Ad 2"
+
+HASHED_PASSWORD = "hashed_password"
+VALID_PASSWORD = "test_password"
+
 VALID_JOB_APPLICATION_ID = uuid.uuid4()
+VALID_PROFESSIONAL_ID = uuid.uuid4()
+VALID_CATEGORY_ID = uuid.uuid4()
 VALID_REQUIREMENT_ID = uuid.uuid4()
 
 VALID_CITY_ID = uuid.uuid4()
@@ -40,4 +48,27 @@ COMPANY = {
     "city": VALID_CITY_NAME,
     "email": VALID_COMPANY_EMAIL,
     "phone_number": VALID_COMPANY_PHONE_NUMBER,
+}
+
+
+JOB_AD = {
+    "id": VALID_JOB_AD_ID,
+    "company_id": VALID_COMPANY_ID,
+    "category_id": VALID_CATEGORY_ID,
+    "location_id": VALID_CITY_ID,
+    "title": VALID_JOB_AD_TITLE,
+    "description": VALID_JOB_AD_DESCRIPTION,
+    "min_salary": 1000.00,
+    "max_salary": 2000.00,
+}
+
+JOB_AD_2 = {
+    "id": VALID_JOB_AD_ID_2,
+    "company_id": VALID_COMPANY_ID,
+    "category_id": VALID_CATEGORY_ID,
+    "location_id": VALID_CITY_ID,
+    "title": VALID_JOB_AD_TITLE_2,
+    "description": VALID_JOB_AD_DESCRIPTION_2,
+    "min_salary": 1200.00,
+    "max_salary": 2300.00,
 }
