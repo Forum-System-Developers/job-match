bc6f664524a6ccc062bcaa137f9da42ad76af81d
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-11-27 00:48:20 +0200

    [DOCS]: Add docstring to get_all function in category_service.

--- src/app/services/category_service.py
+++ src/app/services/category_service.py
@@ -5,5 +5,14 @@ from app.sql_app.category.category import Category
 
 
 def get_all(db: Session) -> list[CategoryResponse]:
+    """
+    Retrieve all categories from the database.
+
+    Args:
+        db (Session): The database session used to query the categories.
+
+    Returns:
+        list[CategoryResponse]: A list of CategoryResponse objects representing all categories in the database.
+    """
     categories = db.query(Category).all()
     return [CategoryResponse.create(category) for category in categories]
