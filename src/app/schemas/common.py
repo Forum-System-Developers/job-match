1772097979c94f7fb70c5ba67fa6ce539174d317
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-02 21:18:47 +0200

    [REFACTOR]: Update JobAdSearchParams to enhance attribute descriptions.

--- src/app/schemas/common.py
+++ src/app/schemas/common.py
@@ -112,25 +112,19 @@ class SearchJobApplication(SearchParams):
 
 class JobAdSearchParams(SearchParams):
     """
-    JobAdSearchParams is a class that defines the parameters for searching job advertisements.
+    JobAdSearchParams is a data model for defining the parameters used in searching job advertisements.
 
     Attributes:
         title (str | None): The title of the job ad. Default is None.
-        min_salary (int | None): Minimum salary for the job ad. Default is None.
-        max_salary (int | None): Maximum salary for the job ad. Default is None.
-        company_id (str | None): The company ID associated with the job ad. Default is None.
-        location_id (str | None): The location ID associated with the job ad. Default is None.
-        job_ad_status (JobAdStatus): The status of the job ad. Default is JobAdStatus.ACTIVE.
-            - ACTIVE: Represents an active job ad.
-            - ARCHIVED: Represents an archived job ad.
+        salary_threshold (float): The salary threshold. Must be greater than or equal to 0. Default is 0.
+        min_salary (float | None): The minimum salary. Must be non-negative and less than or equal to max_salary if provided. Default is None.
+        max_salary (float | None): The maximum salary. Default is None.
+        company_id (UUID | None): The company ID. Default is None.
+        location_id (UUID | None): The location ID. Default is None.
+        job_ad_status (JobAdStatus): The status of the job ad. Can be ACTIVE or ARCHIVED. Default is JobAdStatus.ACTIVE.
+        skills (list[str]): A list of skills to be included in the search. Default is an empty list.
+        skills_threshold (int): The skills threshold. Must be between 0 and the number of skills. Default is 0.
     """
-
-    skills: list[str] | None = Field(
-        examples=[["Python", "Linux", "React"]],
-        default=None,
-        description="List a set of skills to be included in the search",
-    )
-
     title: str | None = Field(description="The title of the job ad", default=None)
     salary_threshold: float = Field(description="The salary threshold", ge=0, default=0)
     min_salary: float | None = Field(description="Minimum salary", default=None)
@@ -140,6 +134,11 @@ class JobAdSearchParams(SearchParams):
     job_ad_status: JobAdStatus = Field(
         description="ACTIVE: Represents an active job ad. ARCHIVED: Represents an archived job ad",
         default=JobAdStatus.ACTIVE,
+    )
+    skills: list[str] = Field(
+        examples=[["Python", "Linux", "React"]],
+        default=[],
+        description="List a set of skills to be included in the search",
     )
     skills_threshold: int = Field(description="The skills threshold", ge=0, default=0)
 
