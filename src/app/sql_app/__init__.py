a738a198eff173046d38a2e8dea0a655d8454eb0
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-03 02:31:39 +0200

    [REFACTOR]: Update imports in __init__.py to include JobAd and adjust __all__ exports accordingly.

--- src/app/sql_app/__init__.py
+++ src/app/sql_app/__init__.py
@@ -4,8 +4,8 @@ from app.sql_app.category_job_application.category_job_application import (
 )
 from app.sql_app.city.city import City
 from app.sql_app.company.company import Company
-from app.sql_app.job_ad.job_ad import JobAd
 from app.sql_app.job_ad_skill.job_ad_skill import JobAdSkill
+from app.sql_app.job_ad.job_ad import JobAd
 from app.sql_app.job_application.job_application import JobApplication
 from app.sql_app.job_application_skill.job_application_skill import JobApplicationSkill
 from app.sql_app.match.match import Match
@@ -18,12 +18,12 @@ __all__ = [
     "City",
     "Company",
     "Professional",
+    "JobApplicationSkill",
     "JobApplication",
+    "JobAdSkill",
     "JobAd",
     "Match",
     "Skill",
     "PendingSkill",
-    "JobAdSkill",
-    "JobApplicationSkill",
     "CategoryJobApplication",
 ]
