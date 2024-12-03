2c6afc91cdc5e2f67bde0e5d96699dd681d31fa7
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-03 02:22:12 +0200

    [REFACTOR]: Update Skill model to rename job_ad_skills to job_ads and adjust relationships accordingly.

--- src/app/sql_app/skill/skill.py
+++ src/app/sql_app/skill/skill.py
@@ -8,17 +8,18 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.sql_app.database import Base
 
 if TYPE_CHECKING:
-    from app.sql_app import JobAdSkill, JobApplicationSkill
+    from app.sql_app import JobAd, JobApplicationSkill
 
 
 class Skill(Base):
     """
     Represents a skill entity in the database.
+
     Attributes:
         id (uuid.UUID): Unique identifier for the skill.
-        category_id (uuid.UUID): Foreign key referencing the category to which the skill belongs.
+        category_id (uuid.UUID): Foreign key referencing the category this skill belongs to.
         name (str): Name of the skill.
-        job_ad_skills (list[JobAdSkill]): List of job advertisement skills associated with this skill.
+        job_ads (list[JobAd]): List of job advertisements associated with this skill.
         job_application_skills (list[JobApplicationSkill]): List of job application skills associated with this skill.
     """
 
@@ -36,9 +37,10 @@ class Skill(Base):
     )
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
 
-    job_ad_skills: Mapped[list["JobAdSkill"]] = relationship(
-        "JobAdSkill",
-        back_populates="skill",
+    job_ads: Mapped[list["JobAd"]] = relationship(
+        "JobAd",
+        secondary="job_ad_skill",
+        back_populates="skills",
         uselist=True,
         collection_class=list,
     )
