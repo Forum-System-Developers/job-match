5111f46017a64b2d557fd3a70436ede73f87611a
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-05 15:27:05 +0200

    [TEST]: Add unit test for creating email messages in MailService.

--- tests/services/mail_service_test.py
+++ tests/services/mail_service_test.py
@@ -1,3 +1,5 @@
+from email.mime.multipart import MIMEMultipart
+
 import pytest
 
 from app.schemas.common import MessageResponse
@@ -12,6 +14,9 @@ SMTP_FROM_EMAIL = "from@example.com"
 TO_EMAIL = "toemail@example.com"
 SUBJECT = "Test Subject"
 MESSAGE = "Test Message"
+
+MESSAGE_ID = "<msg-id>"
+UNSUBSCRIBE_HEADER = "<mailto:@example.com?subject=unsubscribe>"
 
 
 @pytest.fixture
@@ -84,3 +89,32 @@ def test_sendMail_returnsErrorMessage_whenMailSendingFails(mocker, mail_service)
     )
     mock_smtp.assert_called_once_with(SMTP_SERVER, SMTP_PORT)
     assert response == MessageResponse(message=f"Error sending email to {TO_EMAIL}")
+
+
+def test_createMessage_createsMessageWithCorrectFields(mocker, mail_service):
+    # Arrange
+    mock_make_msgid = mocker.patch(
+        "app.services.mail_service.make_msgid", return_value=MESSAGE_ID
+    )
+    mock_generate_unsubscribe_header = mocker.patch.object(
+        mail_service, "_generate_unsubscribe_header", return_value=UNSUBSCRIBE_HEADER
+    )
+
+    # Act
+    message = mail_service._create_message(
+        to_email=TO_EMAIL,
+        subject=SUBJECT,
+        body=MESSAGE,
+    )
+
+    # Assert
+    mock_make_msgid.assert_called_once()
+    mock_generate_unsubscribe_header.assert_called_once_with(None)
+    assert isinstance(message, MIMEMultipart)
+    assert message["From"] == SMTP_FROM_EMAIL
+    assert message["To"] == TO_EMAIL
+    assert message["Subject"] == SUBJECT
+    assert isinstance(message["Date"], str)
+    assert message.get_payload()[0].get_payload() == MESSAGE
+    assert message["Message-ID"] == MESSAGE_ID
+    assert message["List-Unsubscribe"] == UNSUBSCRIBE_HEADER
