0b9feb78606bddf28afee01a98e5e4dbbf7351a1
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-08 23:46:02 +0200

    [REFACTOR]: Add CV file validation to ensure only PDF uploads are accepted.

--- src/app/services/utils/file_utils.py
+++ src/app/services/utils/file_utils.py
@@ -29,3 +29,25 @@ def validate_uploaded_file(uploaded_file: UploadFile) -> None:
             detail=f"File size exceeds the allowed limit of {MAX_FILE_SIZE_MB}MB.",
             status_code=status.HTTP_400_BAD_REQUEST,
         )
+
+
+def validate_uploaded_cv(cv: UploadFile) -> None:
+    """
+    Validate the uploaded CV file.
+
+    This function checks if the uploaded CV file is in PDF format. If the file is not a PDF,
+    it raises an ApplicationError with a 400 Bad Request status code. If the file is a PDF,
+    it proceeds to validate the uploaded file using the validate_uploaded_file function.
+
+    Args:
+        cv (UploadFile): The uploaded CV file to be validated.
+
+    Raises:
+        ApplicationError: If the uploaded file is not a PDF.
+    """
+    if cv.content_type != "application/pdf":
+        raise ApplicationError(
+            detail="Only PDF files are allowed.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+    validate_uploaded_file(uploaded_file=cv)
