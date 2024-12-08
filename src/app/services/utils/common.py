c422fa067172b7bbcd6aca47067a652acb98449a
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-08 21:32:42 +0200

    [FEAT]: Add function to retrieve job application by ID using external API

--- src/app/services/utils/common.py
+++ src/app/services/utils/common.py
@@ -1,12 +1,16 @@
+from uuid import UUID
+
 from fastapi import HTTPException
 
 from app.schemas.company import CompanyResponse
+from app.schemas.job_application import JobApplicationResponse
 from app.schemas.professional import ProfessionalResponse
 from app.utils.request_handlers import perform_get_request
 from tests.services.urls import (
     COMPANY_BY_EMAIL_URL,
     COMPANY_BY_PHONE_NUMBER_URL,
     COMPANY_BY_USERNAME_URL,
+    JOB_APPLICATIONS_BY_ID_URL,
     PROFESSIONAL_BY_EMAIL_URL,
     PROFESSIONAL_BY_USERNAME_URL,
 )
@@ -56,5 +60,17 @@ def get_professional_by_email(email: str) -> ProfessionalResponse | None:
             url=PROFESSIONAL_BY_EMAIL_URL.format(email=email)
         )
         return ProfessionalResponse(**professional)
+    except HTTPException:
+        return None
+
+
+def get_job_application_by_id(
+    job_application_id: UUID,
+) -> JobApplicationResponse | None:
+    try:
+        job_application = perform_get_request(
+            url=JOB_APPLICATIONS_BY_ID_URL.format(job_application_id=job_application_id)
+        )
+        return JobApplicationResponse(**job_application)
     except HTTPException:
         return None
