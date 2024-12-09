4d43edef35d5002acb18fcb92a6fddb11ff12d22
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-09 20:49:18 +0200

    [REFACTOR]: Enhance utility functions with logging and add new methods for job ads and match requests retrieval.

--- src/app/services/utils/common.py
+++ src/app/services/utils/common.py
@@ -1,19 +1,27 @@
+import logging
 from uuid import UUID
 
 from fastapi import HTTPException
 
 from app.schemas.company import CompanyResponse
+from app.schemas.job_ad import JobAdResponse
 from app.schemas.job_application import JobApplicationResponse
+from app.schemas.match import MatchResponse
 from app.schemas.professional import ProfessionalResponse
 from app.utils.request_handlers import perform_get_request
 from tests.services.urls import (
     COMPANY_BY_EMAIL_URL,
     COMPANY_BY_PHONE_NUMBER_URL,
     COMPANY_BY_USERNAME_URL,
+    JOB_AD_BY_ID_URL,
     JOB_APPLICATIONS_BY_ID_URL,
+    MATCH_REQUESTS_BY_ID_URL,
     PROFESSIONAL_BY_EMAIL_URL,
     PROFESSIONAL_BY_USERNAME_URL,
+    PROFESSIONALS_BY_ID_URL,
 )
+
+logger = logging.getLogger(__name__)
 
 
 def get_company_by_username(username: str) -> CompanyResponse | None:
@@ -21,6 +29,7 @@ def get_company_by_username(username: str) -> CompanyResponse | None:
         company = perform_get_request(
             url=COMPANY_BY_USERNAME_URL.format(username=username)
         )
+        logger.info(f"Retrieved company with username {username}")
         return CompanyResponse(**company)
     except HTTPException:
         return None
@@ -29,6 +38,7 @@ def get_company_by_username(username: str) -> CompanyResponse | None:
 def get_company_by_email(email: str) -> CompanyResponse | None:
     try:
         company = perform_get_request(url=COMPANY_BY_EMAIL_URL.format(email=email))
+        logger.info(f"Retrieved company with email {email}")
         return CompanyResponse(**company)
     except HTTPException:
         return None
@@ -39,7 +49,19 @@ def get_company_by_phone_number(phone_number: str) -> CompanyResponse | None:
         company = perform_get_request(
             url=COMPANY_BY_PHONE_NUMBER_URL.format(phone_number=phone_number)
         )
+        logger.info(f"Retrieved company with phone number {phone_number}")
         return CompanyResponse(**company)
+    except HTTPException:
+        return None
+
+
+def get_professional_by_id(professional_id: UUID) -> ProfessionalResponse | None:
+    try:
+        professional = perform_get_request(
+            url=PROFESSIONALS_BY_ID_URL.format(professional_id=professional_id)
+        )
+        logger.info(f"Retrieved professional with id {professional_id}")
+        return ProfessionalResponse(**professional)
     except HTTPException:
         return None
 
@@ -49,6 +71,7 @@ def get_professional_by_username(username: str) -> ProfessionalResponse | None:
         professional = perform_get_request(
             url=PROFESSIONAL_BY_USERNAME_URL.format(username=username)
         )
+        logger.info(f"Retrieved professional with username {username}")
         return ProfessionalResponse(**professional)
     except HTTPException:
         return None
@@ -59,6 +82,7 @@ def get_professional_by_email(email: str) -> ProfessionalResponse | None:
         professional = perform_get_request(
             url=PROFESSIONAL_BY_EMAIL_URL.format(email=email)
         )
+        logger.info(f"Retrieved professional with email {email}")
         return ProfessionalResponse(**professional)
     except HTTPException:
         return None
@@ -71,6 +95,35 @@ def get_job_application_by_id(
         job_application = perform_get_request(
             url=JOB_APPLICATIONS_BY_ID_URL.format(job_application_id=job_application_id)
         )
+        logger.info(f"Retrieved job application with id {job_application_id}")
         return JobApplicationResponse(**job_application)
+    except HTTPException:
+        return None
+
+
+def get_job_ad_by_id(job_ad_id: UUID) -> JobAdResponse | None:
+    try:
+        job_ad = perform_get_request(url=JOB_AD_BY_ID_URL.format(job_ad_id=job_ad_id))
+        logger.info(f"Retrieved job ad with id {job_ad_id}")
+        return JobAdResponse(**job_ad)
+    except HTTPException:
+        return None
+
+
+def get_match_request_by_id(
+    job_ad_id: UUID,
+    job_application_id: UUID,
+) -> MatchResponse | None:
+    try:
+        match = perform_get_request(
+            url=MATCH_REQUESTS_BY_ID_URL.format(
+                job_ad_id=job_ad_id,
+                job_application_id=job_application_id,
+            )
+        )
+        logger.info(
+            f"Retrieved match request for job ad with id {job_ad_id} and job application with id {job_application_id}"
+        )
+        return MatchResponse(**match)
     except HTTPException:
         return None
