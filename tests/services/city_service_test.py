e4c26bc532e9e98a9567adb49001d5ece8cd931e
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-10 22:35:46 +0200

    [TEST]: Refactor city_service tests to use mock requests and improve readability.

--- tests/services/city_service_test.py
+++ tests/services/city_service_test.py
@@ -1,131 +1,74 @@
-from unittest.mock import ANY
-
 import pytest
-from fastapi import status
 
-from app.exceptions.custom_exceptions import ApplicationError
 from app.schemas.city import CityResponse
-from app.services.city_service import get_by_id, get_by_name, get_id
-from app.sql_app.city.city import City
+from app.services import city_service
+from app.services.external_db_service_urls import CITIES_URL, CITY_BY_ID_URL, CITY_BY_NAME_URL
 from tests import test_data as td
-from tests.utils import assert_filter_called_with
 
 
-@pytest.fixture
-def mock_db(mocker):
-    return mocker.Mock()
-
-
-def test_getByName_returns_city_when_city_is_found(mocker, mock_db):
+def test_getByName_returnsCity_whenCityExists(mocker) -> None:
     # Arrange
-    city = mocker.Mock()
-    city.id = td.VALID_CITY_ID
-    city.name = td.VALID_CITY_NAME
-
-    mock_query = mock_db.query.return_value
-    mock_filter = mock_query.filter.return_value
-    mock_filter.first.return_value = city
-
-    expected_city_response = CityResponse(id=td.VALID_CITY_ID, name=td.VALID_CITY_NAME)
+    city = td.CITY
+    mock_perform_get_request = mocker.patch(
+        "app.services.city_service.perform_get_request",
+        return_value=city,
+    )
+    mock_city_response = mocker.patch(
+        "app.services.city_service.CityResponse",
+        return_value=mocker.Mock(spec=CityResponse),
+    )
 
     # Act
-    result = get_by_name(city_name=td.VALID_CITY_NAME, db=mock_db)
+    result = city_service.get_by_name(city_name=td.VALID_CITY_NAME)
 
     # Assert
-    mock_db.query.assert_called_once_with(City)
-    mock_query.filter.assert_called_once_with(ANY)
-    assert_filter_called_with(mock_query, City.name == td.VALID_CITY_NAME)
-    assert result == expected_city_response
+    mock_perform_get_request.assert_called_once_with(
+        url=CITY_BY_NAME_URL.format(city_name=td.VALID_CITY_NAME)
+    )
+    mock_city_response.assert_called_once_with(**city)
+    assert isinstance(result, CityResponse)
 
 
-def test_getByName_raises_error_when_city_is_not_found(mock_db):
+def test_getById_returnsCity_whenCityExists(mocker) -> None:
     # Arrange
-    mock_query = mock_db.query.return_value
-    mock_filter = mock_query.filter.return_value
-    mock_filter.first.return_value = None
+    city = td.CITY
+    mock_perform_get_request = mocker.patch(
+        "app.services.city_service.perform_get_request",
+        return_value=city,
+    )
+    mock_city_response = mocker.patch(
+        "app.services.city_service.CityResponse",
+        return_value=mocker.Mock(spec=CityResponse),
+    )
 
     # Act
-    with pytest.raises(ApplicationError) as exc:
-        get_by_name(city_name=td.VALID_CITY_NAME, db=mock_db)
+    result = city_service.get_by_id(city_id=td.VALID_CITY_ID)
 
     # Assert
-    mock_db.query.assert_called_once_with(City)
-    assert_filter_called_with(mock_query, City.name == td.VALID_CITY_NAME)
-    assert exc.value.data.status == status.HTTP_404_NOT_FOUND
-    assert exc.value.data.detail == f"City with name {td.VALID_CITY_NAME} was not found"
+    mock_perform_get_request.assert_called_once_with(
+        url=CITY_BY_ID_URL.format(city_id=td.VALID_CITY_ID)
+    )
+    mock_city_response.assert_called_once_with(**city)
+    assert isinstance(result, CityResponse)
 
-
-def test_getById_returns_city_when_city_is_found(mocker, mock_db):
+def test_getAll_returnsListOfCities_whenCitiesExist(mocker) -> None:
     # Arrange
-    city = mocker.Mock()
-    city.id = td.VALID_CITY_ID
-    city.name = td.VALID_CITY_NAME
-
-    mock_query = mock_db.query.return_value
-    mock_filter = mock_query.filter.return_value
-    mock_filter.first.return_value = city
-
-    expected_city_response = CityResponse(id=td.VALID_CITY_ID, name=td.VALID_CITY_NAME)
+    cities = [td.CITY, td.CITY_2]
+    mock_perform_get_request = mocker.patch(
+        "app.services.city_service.perform_get_request",
+        return_value=cities,
+    )
+    mock_city_response = mocker.patch(
+        "app.services.city_service.CityResponse",
+        side_effect=[mocker.Mock(spec=CityResponse), mocker.Mock(spec=CityResponse)],
+    )
 
     # Act
-    result = get_by_id(city_id=td.VALID_CITY_ID, db=mock_db)
+    result = city_service.get_all()
 
     # Assert
-    mock_db.query.assert_called_once_with(City)
-    mock_query.filter.assert_called_once_with(ANY)
-    assert_filter_called_with(mock_query, City.id == td.VALID_CITY_ID)
-    assert result == expected_city_response
-
-
-def test_getById_raises_error_when_city_is_not_found(mock_db):
-    # Arrange
-    mock_query = mock_db.query.return_value
-    mock_filter = mock_query.filter.return_value
-    mock_filter.first.return_value = None
-
-    # Act
-    with pytest.raises(ApplicationError) as exc:
-        get_by_id(city_id=td.VALID_CITY_ID, db=mock_db)
-
-    # Assert
-    mock_db.query.assert_called_once_with(City)
-    assert_filter_called_with(mock_query, City.id == td.VALID_CITY_ID)
-    assert exc.value.data.status == status.HTTP_404_NOT_FOUND
-    assert exc.value.data.detail == f"City with id {td.VALID_CITY_ID} was not found"
-
-
-def test_getId_returns_city_id_when_city_is_found(mocker, mock_db):
-    # Arrange
-    city = mocker.Mock()
-    city.id = td.VALID_CITY_ID
-    city.name = td.VALID_CITY_NAME
-
-    mock_query = mock_db.query.return_value
-    mock_filter = mock_query.filter.return_value
-    mock_filter.first.return_value = city
-
-    # Act
-    result = get_id(db=mock_db, city_name=td.VALID_CITY_NAME)
-
-    # Assert
-    mock_db.query.assert_called_once_with(City)
-    mock_query.filter.assert_called_once_with(ANY)
-    assert_filter_called_with(mock_query, City.name == td.VALID_CITY_NAME)
-    assert result == td.VALID_CITY_ID
-
-
-def test_getId_raises_error_when_city_is_not_found(mock_db):
-    # Arrange
-    mock_query = mock_db.query.return_value
-    mock_filter = mock_query.filter.return_value
-    mock_filter.first.return_value = None
-
-    # Act
-    with pytest.raises(ApplicationError) as exc:
-        get_id(db=mock_db, city_name=td.VALID_CITY_NAME)
-
-    # Assert
-    mock_db.query.assert_called_once_with(City)
-    assert_filter_called_with(mock_query, City.name == td.VALID_CITY_NAME)
-    assert exc.value.data.status == status.HTTP_404_NOT_FOUND
-    assert exc.value.data.detail == f"City with name {td.VALID_CITY_NAME} not found."
+    mock_perform_get_request.assert_called_once_with(url=CITIES_URL)
+    assert mock_city_response.call_count == len(cities)
+    assert all(isinstance(city, CityResponse) for city in result)
+    assert len(result) == len(cities)
+    
